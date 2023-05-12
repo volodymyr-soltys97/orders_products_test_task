e5d25f3b86ec57b79c72d00079e729832f147565
Volodymyr Soltys <soltus.vovan@gmail.com>
2023-05-12 11:52:32 +0300

    add compponent for menu

---
+++ src/components/MainMenu/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const MainMenu = () => {
+
+    const items = [
+        {
+            id: 1,
+            title: "",
+            link: "",
+        },
+        {
+            id: 2,
+            title: "",
+            link: "",
+        },
+        {
+            id: 3,
+            title: "",
+            link: "",
+        },
+        {
+            id: 4,
+            title: "",
+            link: "",
+        },
+        {
+            id: 5,
+            title: "",
+            link: "",
+        }
+    ];
+
+    return (
+        <>
+        </>
+    );
+};
+
+export default MainMenu;
